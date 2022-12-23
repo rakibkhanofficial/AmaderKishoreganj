5d85476bf448acd254364012542dcc9ab92d16c1
Rakib Khan <khanrakib218@gmail.com>
2022-12-23 10:36:02 +0600

    update

--- app/src/main/java/com/bengaldroid/amaderkishoreganj/SecondFragment.java
+++ app/src/main/java/com/bengaldroid/amaderkishoreganj/SecondFragment.java
@@ -7,7 +7,6 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
 import com.bengaldroid.amaderkishoreganj.databinding.FragmentSecondBinding;
 
@@ -17,7 +16,7 @@ public class SecondFragment extends Fragment {
 
     @Override
     public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
+            @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
 
@@ -26,17 +25,7 @@ public class SecondFragment extends Fragment {
 
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
-            }
-        });
-    }
 
     @Override
     public void onDestroyView() {
